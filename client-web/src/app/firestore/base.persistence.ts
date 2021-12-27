6d5cff745c689fcdc543c88392c005520807fd5f
khmy2010 <kehan.see@gmail.com>
2021-12-27 17:50:56 +0800

    WIP in base persistence layer.

--- client-web/src/app/firestore/base.persistence.ts
+++ client-web/src/app/firestore/base.persistence.ts
@@ -1,21 +1,40 @@
 import { Inject, Injectable } from "@angular/core";
-import { doc, docData, Firestore, addDoc, collection, CollectionReference } from '@angular/fire/firestore';
+import { Auth } from "@angular/fire/auth";
+import { doc, docData, Firestore, addDoc, collection, query, where, CollectionReference, serverTimestamp } from '@angular/fire/firestore';
 
 @Injectable()
 export class BasePersistenceService<T> {
   private collectionRef: CollectionReference = collection(this.firestore, this.collectionID);
 
-  constructor(@Inject(String) protected collectionID: string, protected firestore: Firestore) {
+  constructor(
+    @Inject(String) protected collectionID: string, 
+    protected firestore: Firestore,
+    protected auth: Auth) {
 
   }
 
   add(payload: T) {
     const dataPayload: T = {
-      ...payload
+      ...payload,
+      uid: this.getUserId(),
+      createdDate: serverTimestamp()
+    };
+
+    return addDoc(this.collectionRef, {...dataPayload});
+  }
+
+  addByUser(payload: T) {
+    const dataPayload: T = {
+      ...payload,
+      uid: this.getUserId(),
+      createdDate: serverTimestamp(),
+      createdBy: this.getUserId()
     };
 
     return addDoc(this.collectionRef, {...dataPayload});
   }
   
-  
+  getUserId(): string | null {
+    return this.auth.currentUser?.uid ?? null;
+  }
 }
--- client-web/src/app/firestore/persistence/categories.service.ts
+++ client-web/src/app/firestore/persistence/categories.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Auth } from "@angular/fire/auth";
 import { Firestore } from "@angular/fire/firestore";
 import { FireCollectionConstant } from "src/constant";
 import { BasePersistenceService } from "../base.persistence";
@@ -8,7 +9,7 @@ import { FCategoryModel } from "../model/store.model";
   providedIn: 'root'
 })
 export class CategoriesStoreService extends BasePersistenceService<FCategoryModel> {
-  constructor(protected override firestore: Firestore) {
-    super(FireCollectionConstant.CATEGORIES, firestore);
+  constructor(protected override firestore: Firestore, protected override auth: Auth) {
+    super(FireCollectionConstant.CATEGORIES, firestore, auth);
   }
 }
