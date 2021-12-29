83ddc045c83c95280591c2bcb08d25c673ed6d15
khmy2010 <kehan.see@gmail.com>
2021-12-29 15:11:31 +0800

    setup firebase cloud functions deploy workflow.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+export const createUserStore = functions.auth.user().onCreate(async (user: any) => {
+  const { uid, displayName, email, photoURL } = user;
+
+  console.log(`Attemping to create user ${uid}`);
+  const now = admin.firestore.Timestamp.now();
+
+  await firestore.collection(`users`).doc(uid).set({
+    uid,
+    displayName,
+    email,
+    photoURL,
+    createdDate: now,
+    lastLogin: now,
+  });
+
+  console.log(`Attempting to create a default payment method for user ${uid}`);
+  const defaultPaymentMethod: any = {
+    name: 'Cash',
+    type: 'cash',
+    uid,
+    createdDate: now,
+    status: 'active',
+  };
+
+  await firestore.collection('payment-methods').add(defaultPaymentMethod);
+
+  console.log(user.uid + ' has been created in Firestore.');
 });
